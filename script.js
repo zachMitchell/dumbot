8b49bfa609406296e5e873296c01c712ee87d217
Zachary <31785316+zachMitchell@users.noreply.github.com>
2020-06-28 21:29:02 -0400

    Far from done here, but getting there!
    shook answer.js to it's core and organized EVERYTHING.
    
    Still need to properly get rid of the "count" variable

--- script.js
+++ script.js
@@ -1,50 +1,36 @@
+//the 6 core beginning words of a question, plus everything else (:P) :
+
+var grammarConstructs = [
+    ["Who", "What?!", "When", "Where", "Why", "How", ""],
+    ["could", "would", "did", "should", "might", "must", "may", "can", "did", "does", "will"],
+    ["you", "your dog", "your brother", "your sister", "Dad", "Mom", "Grandma", "Grandpa", "your cat", "your fish", "your friend", "Chuck Noris", "an athlete", "a chef", "a surgeon", "a clown", "a spec of dust", "pizza", "somebody", "that guy", "the government", "the planet", "a bird", "aliens", "a computer", "your dentist", "the President", "the King", "the mechanic", "we", "they", "that cuddly creature", "that vampire", "your phone"],
+    ["eat", "explode", "kill", "take care of", "drive to", "fly to", "swim to", "jump to", "dominate over", "lose to", "make", "bake", "flatten", "break into", "comment about", "race", "feed", "jump over", "teleport to", "construct", "pickle", "pester", "flip over", "advertise", "play with", "conquer", "become", "obtain", "infiltrate", "question", "plead to", "capture", "steal", "crave", "read about", "watch", "fight", "fight over", "cry over", "think about", "fetch", "clean", "repair", "run over", "punch", "karate-chop", "drink", "buy", "have", "chuck", "cuddle", "yell at", "fall off of", "topple over"],
+    ["a burger", "a videogame", "a coin", "a pepper", "a house", "the world", "playing cards", "a violin", "the President", "a portal gun", "water", "air", "the beach", "mars", "the moon", "your teacher", "a bubble", "the ocean", "a car", "China", "your computer", "doritoes", "awesomeness", "a book", "an imagination", "life", "death", "the center of the earth", "hotdogs", "dust bunnies", "a helicopter", "a tank", "the universe", "math", "homework", "cheese", "you", "a superhero", "strength", "weakness", "candy", "ice cream", "a pirate", "the brain", "school", "the playground", "us", "them", "the squirrel", "steak", "pork-chop", "authority", "breakfast", "a vampire", "hair", "nothing", "everything", "a porqupine", "outer space", "War and Peace"]
+];
+
 function compute() {
     pollbar(0);
 
-    //These random numbers will work without maintanence... XP
-    s1r = Math.floor(Math.random() * step1.length);
-    s2r = Math.floor(Math.random() * step2.length);
-    s3r = Math.floor(Math.random() * step3.length);
-    s4r = Math.floor(Math.random() * step4.length);
-    s5r = Math.floor(Math.random() * step5.length);
+    var punctuation = '?';
 
-    //Determines if step 2 should be capitalized
-    var step2cap = "";
-    var step2S = step2[s2r];
+    //Grab a random phrase for each part of the sentence
+    var sentence = [];
+    for(var construct of grammarConstructs)
+        sentence.push(construct[rnd(construct.length)])
 
-    if (s1r == 1 || s1r == 6) {
-        for (i = 0; i < step2[s2r].length; i++) {
-            if (i == 0) {
-                step2cap += step2[s2r][i].toUpperCase();
-            }
-            else {
-                step2cap += step2[s2r][i];
-            }
-        }
-        step2S = step2cap;
+    //Determines if step 2 should be capitalized ("What" or blank string triggers this)
+    var isWhat = sentence[0] == grammarConstructs[0][1];
+    if(isWhat || !sentence[0].length){
+        sentence[1] = sentence[1][0].toUpperCase()+sentence[1].substring(1);
+        if(isWhat) punctuation+='!'; //If the beginning is "What?!", keep the theme going ;P
     }
-    //if step 1 is "who", then step five is blank...
-    if (s1r == 0) {
-        s5r = 0;
-    }
-    //...also the spacebar is removed.
-    var spacebar = " ";
-
-    if (step5[s5r] === step5[0]) {
-        spacebar = '';
+    //if index 1 is "who", then remove 5th construct...
+    else if(sentence[0] == grammarConstructs[0][0]){
+        sentence.pop();
     }
 
-    //The "what" questions are unique, requiring rephrasing of the sentence, this resolves the problem.
-    if (s1r == 1) {
-        step6 = "?!";
-    }
-    else {
-        step6 = "?";
-    }
-    //the final step: create the question!
-    var text = step1[s1r] + " " + step2S + " " + step3[s3r] + " " + step4[s4r] + spacebar + step5[s5r] + step6;
-
-    questionElement.innerHTML = text;
-    printAnswer();
+    //Insert question:
+    questionElement.innerHTML = sentence.join(' ')+punctuation;;
+    printAnswer(sentence);
 }
 compute();
